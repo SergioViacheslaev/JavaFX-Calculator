7f6524411896a37ee230a50d46211dcdce7ec703
SergioViacheslaev <internetwalker@yandex.ru>
2020-03-16 19:08:26 +0300

    Initial

---
+++ src/main/java/org/home/calculator/App.java
@@ -0,0 +1,44 @@
+package org.home.calculator;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+/**
+ * JavaFX App
+ */
+public class App extends Application {
+
+    private static Scene scene;
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        scene = new Scene(loadFXML("primary"));
+        stage.setScene(scene);
+        stage.getIcons().add(new Image("static/images/logo.png"));
+        stage.setMinHeight(400);
+        stage.setMinWidth(700);
+//        stage.setResizable(false);
+        stage.show();
+    }
+
+    public static void setRoot(String fxml) throws IOException {
+        scene.setRoot(loadFXML(fxml));
+    }
+
+    private static Parent loadFXML(String fxml) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("/view/" + fxml + ".fxml"));
+        return fxmlLoader.load();
+    }
+
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+}
---
+++ src/main/java/org/home/calculator/Main.java
@@ -0,0 +1,7 @@
+package org.home.calculator;
+
+public class Main {
+    public static void main(String[] args) {
+        App.main(args);
+    }
+}
