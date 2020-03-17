8f2a51e031d99600b67b558193bd528d1ffb5f7c
SergioViacheslaev <internetwalker@yandex.ru>
2020-03-17 15:13:06 +0300

    Add backspace action handling.

--- src/main/java/org/home/calculator/controllers/CalculatorController.java
+++ src/main/java/org/home/calculator/controllers/CalculatorController.java
@@ -22,7 +22,29 @@ public class CalculatorController {
     private Calculation calculation;
     private boolean start = false;
     private long number1 = 0L;
-    private String operator;
+    private String operator = "";
+    private String previousResult = "";
+
+    @FXML
+    void initialize() {
+        calculation = new Calculation();
+    }
+
+
+    /**
+     * Handles backspace button action.
+     *
+     * @param event
+     */
+    @FXML
+    void handleBackspaceAction(ActionEvent event) {
+        String currentResult = resultField.getText();
+        if (currentResult.length() != 1) {
+            resultField.setText(currentResult.substring(0, currentResult.length() - 1));
+        }
+    }
+
+
 
     @FXML
     void processControls(ActionEvent event) {
@@ -30,8 +52,7 @@ public class CalculatorController {
 
         switch (controlButtonName) {
             case "ON / C":
-                resultField.setText("0");
-                start = true;
+                cleanAllResults();
                 break;
             case ",":
                 resultField.setText(resultField.getText() + ",");
@@ -53,31 +74,36 @@ public class CalculatorController {
 
     @FXML
     void processOperators(ActionEvent event) {
-        String operatorValue = ((Button) event.getSource()).getText();
+        String currentOperator = ((Button) event.getSource()).getText();
 
-        if (!operatorValue.equals("=")) {
-            operator = operatorValue;
+        if (!currentOperator.equals("=")) {
+            operator = currentOperator;
             number1 = Long.parseLong(resultField.getText());
             start = true;
         } else {
-            long number2 = Long.parseLong(resultField.getText());
+            if (!operator.equals("")) {
+                long number2 = Long.parseLong(resultField.getText());
 
-            if (operator.equals("/") && number2 == 0) {
-                resultField.setText("На ноль делить нельзя !");
-                return;
+                if (operator.equals("/") && number2 == 0) {
+                    resultField.setText("На ноль делить нельзя !");
+                    return;
+                }
+
+
+                long calcResult = calculation.compute(number1, number2, operator);
+                resultField.setText(String.valueOf(calcResult));
             }
 
-
-            long calcResult = calculation.compute(number1, number2, operator);
-            resultField.setText(String.valueOf(calcResult));
         }
 
 
     }
 
-    @FXML
-    void initialize() {
-        calculation = new Calculation();
+
+    private void cleanAllResults() {
+        resultField.setText("0");
+        start = true;
+        operator = "";
     }
 
 
